f19a7217f3dfd63ab8a1f3828945f34602a2a250
mitzyun <mqy2@georgetown.edu>
2022-03-22 14:50:30 -0400

    complete? maybe some testing

--- app/src/main/java/com/example/coffeemapper/MainActivity.kt
+++ app/src/main/java/com/example/coffeemapper/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -82,45 +83,69 @@ class MainActivity : AppCompatActivity() {
                     intent.putExtra("placeNumber", position)
                     startActivity(intent)
                 } else {
-                    val intent = Intent(applicationContext, CoffeeDetailActivity::class.java)
-                    intent.putExtra("placeNumber", position)
+                    //val intent = Intent(applicationContext, CoffeeDetailActivity::class.java)
+                    //intent.putExtra("placeNumber", position)
+                    cafeDetail(position)
                 }
             }
         listView.onItemLongClickListener =
             AdapterView.OnItemLongClickListener { adapterView, view, i, l ->
-
                 val itemDelete = i
 
                 AlertDialog.Builder(this)
                     .setMessage("Are you sure you want to delete this cafe?")
                     .setPositiveButton("Yes") { dialogInterface: DialogInterface, i: Int ->
 
-                        places.removeAt(itemDelete)
-                        latitudes.removeAt(itemDelete)
-                        longitudes.removeAt(itemDelete)
-                        names.removeAt(itemDelete)
-                        ratings.removeAt(itemDelete)
-
-                        arrayAdapter!!.notifyDataSetChanged()
-                        sharedPreferences.edit()
-                            .putString("places", ObjectSerializer.serialize(places))
-                            .apply()
-                        sharedPreferences.edit()
-                            .putString("lats", ObjectSerializer.serialize(latitudes))
-                            .apply()
-                        sharedPreferences.edit()
-                            .putString("lons", ObjectSerializer.serialize(longitudes))
-                            .apply()
-                        sharedPreferences.edit()
-                            .putString("names", ObjectSerializer.serialize(names))
-                            .apply()
-                        sharedPreferences.edit()
-                            .putString("ratings", ObjectSerializer.serialize(ratings))
-                            .apply()
+                        deleteCafe(i)
                     }//setPositiveButton
                     .setNegativeButton("No", null)
                     .show()
+
                 return@OnItemLongClickListener true
             }
     }//onCreate
+
+    fun deleteCafe(itemDelete: Int) {
+        places.removeAt(itemDelete)
+        latitudes.removeAt(itemDelete)
+        longitudes.removeAt(itemDelete)
+        names.removeAt(itemDelete)
+        ratings.removeAt(itemDelete)
+
+        arrayAdapter!!.notifyDataSetChanged()
+        val sharedPreferences = getSharedPreferences("com.example.coffeemapper", Context.MODE_PRIVATE)
+        sharedPreferences.edit()
+            .putString("places", ObjectSerializer.serialize(places))
+            .apply()
+        sharedPreferences.edit()
+            .putString("lats", ObjectSerializer.serialize(latitudes))
+            .apply()
+        sharedPreferences.edit()
+            .putString("lons", ObjectSerializer.serialize(longitudes))
+            .apply()
+        sharedPreferences.edit()
+            .putString("names", ObjectSerializer.serialize(names))
+            .apply()
+        sharedPreferences.edit()
+            .putString("ratings", ObjectSerializer.serialize(ratings))
+            .apply()
+    }
+
+    fun cafeDetail(position: Int) {
+        var message: String = "Rating: "
+        message += ratings[position].toString()
+        message += "\nAddress: "
+        message += places[position].toString()
+
+        AlertDialog.Builder(this)
+            .setIcon(R.drawable.coffee)
+            .setTitle(names[position].toString())
+            .setMessage(message)
+            .setPositiveButton("Close", null)
+            .setNegativeButton("Delete") { dialogInterface: DialogInterface, x: Int ->
+                deleteCafe(position)
+            }
+            .show()
+
+    } // cafeDetail
 }//MainActivity
